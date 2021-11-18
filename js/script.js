34bf8a9a623f3255b713c5ddbec36215b9c0500d
Marco <marco.cusenza.me@gmail.com>
2021-11-18 03:15:45 +0100

    aggiunta bombe, sistema a matrice funzionante

--- js/script.js
+++ js/script.js
@@ -2,34 +2,75 @@ const grid = document.getElementById("grid");
 let difficulty = 0; // 0=EASY 1=MEDIUM 2=HARD
 let numBox = 49;
 let boxPerRow = 7;
+let numBombs = 10;
 
 const easy = document.querySelector(".easyBtn");
 const medium = document.querySelector(".mediumBtn");
 const hard = document.querySelector(".hardBtn");
-
+const crazy = document.querySelector(".crazyBtn");
 
 //Inizializzazione griglia modalità easy
 grid.className = "easyGrid";
 fillGrid(7);
+bombsGenerator(numBox, numBombs);
 
 
-//Controllo sul click del cambio difficolta --> nuova partita
+//Cambio difficoltà --> nuova partita
 easy.addEventListener('click', function () { changeDifficulty(0) });
 medium.addEventListener('click', function () { changeDifficulty(1) });
 hard.addEventListener('click', function () { changeDifficulty(2) });
+crazy.addEventListener('click', function () { changeDifficulty(3) });
 
 
 
+//FUNZIONI
+
+//Cambio difficoltà e nuova griglia con nuovo riempimento
+function changeDifficulty(lvl) {
+    if (lvl == 0) {//***livello facile
+        numBox = 49;
+        boxPerRow = 7;
+        numBombs = 10;
+        grid.className = "easyGrid";
+        grid.innerHTML = ""; //svuota la griglia
+        fillGrid(boxPerRow); //riempi la griglia
+        bombsGenerator(numBox, numBombs);
+    } else if (lvl == 1) {//***livello medio
+        numBox = 121;
+        boxPerRow = 11;
+        numBombs = 25;
+        grid.className = "mediumGrid";
+        grid.innerHTML = ""; //svuota la griglia
+        fillGrid(boxPerRow); //riempi la griglia
+        bombsGenerator(numBox, numBombs);
+    } else if (lvl == 2) {//***livello difficile
+        numBox = 225;
+        boxPerRow = 15;
+        numBombs = 50;
+        grid.className = "hardGrid";
+        grid.innerHTML = ""; //svuota la griglia
+        fillGrid(boxPerRow); //riempi la griglia
+        bombsGenerator(numBox, numBombs);
+    } else {//***livello crazy
+        numBox = 625;
+        boxPerRow = 25;
+        numBombs = 100;
+        grid.className = "crazyGrid";
+        grid.innerHTML = ""; //svuota la griglia
+        fillGrid(boxPerRow); //riempi la griglia
+        bombsGenerator(numBox, numBombs);
+    }
+}
 
 //Riempimento griglia
 function fillGrid(nItems) {
     for (let i = 0; i < nItems; i++) {
         for (let j = 0; j < nItems; j++) {
             const box = document.createElement('div');
-            box.className = 'box';
+            box.className = `box box-${positionCalc(i, j, boxPerRow)} raw-${i} col-${j}`;
             //Classe speciale per elementi della prima e ultima riga
             if (i == 0) {
-                box.classList.add("noTop");
+                box.classList.add(`noTop`);
             }
             if (i == nItems - 1) {
                 box.classList.add("noBot");
@@ -51,25 +92,29 @@ function fillGrid(nItems) {
     }
 }
 
-//Cambio difficoltà e nuova griglia con nuovo riempimento
-function changeDifficulty(lvl) {
-    if (lvl == 0) {
-        numBox = 49;
-        boxPerRow = 7;
-        grid.className = "easyGrid";
-        grid.innerHTML = ""; //svuota la griglia
-        fillGrid(boxPerRow); //riempi la griglia
-    } else if (lvl == 1) {
-        numBox = 81;
-        boxPerRow = 9;
-        grid.className = "mediumGrid";
-        grid.innerHTML = ""; //svuota la griglia
-        fillGrid(boxPerRow); //riempi la griglia
-    } else {
-        numBox = 100;
-        boxPerRow = 10;
-        grid.className = "hardGrid";
-        grid.innerHTML = ""; //svuota la griglia
-        fillGrid(boxPerRow); //riempi la griglia
+//generazione e inserimento bombe
+function bombsGenerator(box, bombs) {
+    const array = [];
+    for (let i = 0; i < bombs; i++) {
+        let token = Math.floor(Math.random() * (box - 1));
+        if (array.includes(token)) {
+            i--;
+        } else {
+            array[i] = token;
+        }
     }
+    arrayBox = document.getElementsByClassName('box');
+    for (let j = 0; j < bombs; j++) {
+        arrayBox[array[j]].classList.add("bomb");
+    }
+}
+
+
+//calcolo della posizione di box in un sistema a matrice
+function positionCalc(raw, col, tot) {
+    return ((raw * tot) + col)
+}
+
+function numBombsAround() {
+
 }
